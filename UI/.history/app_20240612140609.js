4ecc136a744ccfb3f688ac14d8a024f2f53275f5
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-06-27 13:04:23 +0530

    completed sports person classifier using ml

---
+++ UI/.history/app_20240612140609.js
@@ -0,0 +1,42 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: imageData
+        },function(data, status) {
+            console.log(data);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
